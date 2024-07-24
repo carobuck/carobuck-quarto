566975f34b9279a58f82b27f59d1975182a17d5d
Caroline Buck <carobuck@Carolines-MacBook-Air.local>
2024-07-24 18:33:57 -0400

    fix broken imgs, google auth, book log

---
+++ google_oauth.R
@@ -0,0 +1,25 @@
+## Quick lil script to set up google drive o-auth for connecting to reading sheet
+# https://gargle.r-lib.org/articles/non-interactive-auth.html#arrange-for-an-oauth-user-token-to-be-re-discovered
+
+# NOTE: many issues w/ getting google to not think this was a scammy request (app kept getting blocked). reinstalling googledrive package eventually helped after redoing the process several times *eyeroll*
+install.packages("googledrive")
+library(googledrive)
+
+# trigger auth on purpose --> store a token in the specified cache
+drive_auth(cache = ".secrets")
+
+library(googledrive)
+
+options(
+  gargle_oauth_cache = ".secrets",
+  gargle_oauth_email = "*@gmail.com"
+)
+
+# now use googledrive with no need for explicit auth
+drive_find(n_max = 5)
+
+
+# Turns out, just make the google sheet public to anyone w/ the link and my problems are resolved *rolls eyes* 
+# a bit annoying to have gone through all the gargle/o-auth stuff and it doesn't work (could get it to work locally but not in quarto preview for some reason)
+# adding the book log url as a github secret so it's not as easily readable/findable
+# https://stackoverflow.com/questions/56224610/storing-and-retrieving-oauth-token-used-by-api-package-rprofile-vs-renviron
